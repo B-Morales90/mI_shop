6f0386ca5b6be88b8ca48097b0699d22edcb16e5
Berlin <dragonfly.warrior90@gmail.com>
2018-12-11 11:03:24 -0600

    dec11
    fixes

--- models.rb
+++ models.rb
@@ -1,3 +1,8 @@
+require 'data_mapper' # metagem, requires common plugins too.
+
+
+# need install dm-sqlite-adapter
+# if on heroku, use Postgres database
 # if not use sqlite3 database I gave you
 if ENV['DATABASE_URL']
   DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
